0ba77e4df27787886c3c9a1bb4a0b020fd38fb7c
Georgi Ivanov <georgi@ouzi.dev>
2020-07-20 16:12:01 +0100

    feat: add cli that checks the validity of avro schemas (#31)
    * feat: add cli that checks the validity of avro schemas
    
    Signed-off-by: Georgi Ivanov <georgi@ouzi.dev>
    
    * fix: update dependencies
    
    Signed-off-by: Georgi Ivanov <georgi@ouzi.dev>

---
+++ cli/cmd/verifySchema.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"io/ioutil"
+
+	"github.com/ouzi-dev/avro-kedavro/pkg/kedavro"
+	"github.com/spf13/cobra"
+)
+
+var schemaFilePath = ""
+
+// verifySchemaCmd represents the verifySchema command
+var verifySchemaCmd = &cobra.Command{
+	Use:   "verifySchema",
+	Short: "Verifies an avro schema is syntactically correct",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		data, err := ioutil.ReadFile(schemaFilePath)
+		if err != nil {
+			// Error reading file
+			return err
+		}
+
+		_, err = kedavro.NewParser(string(data), kedavro.WithStringToNumber(), kedavro.WithTimestampToMillis())
+		if err != nil {
+			// Error parsing schema
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(verifySchemaCmd)
+
+	verifySchemaCmd.Flags().StringVarP(&schemaFilePath, "schema-file-path", "s", "", "The path to the schema file to check")
+	_ = verifySchemaCmd.MarkFlagRequired("schema-file-path")
+}
